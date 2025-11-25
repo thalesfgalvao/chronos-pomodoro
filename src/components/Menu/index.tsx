c92820b430011e7dfad196a33cae327c04cfad0f
Thales Marques <thalesf.galvao@gmail.com>
2025-11-25 11:05:15 -0300

    Feat: change theme Icon in menu option when change theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,42 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import S from './styles.module.css';
 import { useEffect, useState } from 'react';
 
 type AvaliableThemes = 'dark' | 'light';
 
 export const Menu = () => {
-  const [theme, setTheme] = useState<AvaliableThemes>('dark');
+  const [theme, setTheme] = useState<AvaliableThemes>(() => {
+    const storageTheme = //Variavel que pega vallor do  localStorage do useEffect
+      (localStorage.getItem('theme') as AvaliableThemes) || 'dark'; //Pode retornar null ou string caso eu não passe  o type. O pipe serve pra padronizar o dark
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    light: <MoonIcon />,
+    dark: <SunIcon />,
+  };
+
   const handleThemeChange = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, //Essa ação impede que uma nova aba seja aberta no evento onClick
   ) => {
     event.preventDefault();
 
     setTheme(prevTheme => {
+      //Estou setando o theme anterior como se fosse uma unção negando !theme
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
   };
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.setAttribute('data-theme', theme); //data-theme está no index.html
+    localStorage.setItem('theme', theme); //Guardo o theme no localStorage do navegadorr
     return () => {
       console.log('Atualizando este componente');
     };
@@ -52,7 +70,7 @@ export const Menu = () => {
           title='Mudar tema'
           onClick={event => handleThemeChange(event)}
         >
-          <SunIcon />
+          {nextThemeIcon[theme]}
         </a>
       </nav>
     </>
