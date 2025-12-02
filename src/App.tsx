1815bf82ec4baabf96942c561a44efbdfd6931fb
Thales Marques <thalesf.galvao@gmail.com>
2025-12-01 23:06:07 -0300

    Fix:  remove initialState values aan remove useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useState } from 'react';
-import type { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 import { TaskContextProvider } from './contexts/TaskContext';
 
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemainig: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
-
 export const App = () => {
-  const [state, setState] = useState(initialState);
   return (
     <TaskContextProvider>
       <Home />;
